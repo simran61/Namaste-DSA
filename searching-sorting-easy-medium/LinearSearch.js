50cd5dbdeea59d0ae9bcd3cbac035d2cab7815fa
simran61 <simrangangwani3107@gmail.com>
2026-08-24 14:07:36 +0530

    linear search

---
+++ searching-sorting-easy-medium/LinearSearch.js
@@ -0,0 +1,12 @@
+// QUES: Search for an element in an array using Linear Search
+
+let arr = [4, 9, 1, 0, 2];
+
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == target) return i;
+  }
+  return -1;
+}
+
+console.log(linearSearch(arr, 0));
