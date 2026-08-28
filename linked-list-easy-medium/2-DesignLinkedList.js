cd5ccaeaa5c8d53969ede3753f435bb1bcec00b8
simran61 <simrangangwani3107@gmail.com>
2026-08-29 02:09:42 +0530

    deleting & get nodes in linked list

--- linked-list-easy-medium/2-DesignLinkedList.js
+++ linked-list-easy-medium/2-DesignLinkedList.js
@@ -27,7 +27,7 @@ function MyLinkedList() {
 // 3. Adding node at head
 
 MyLinkedList.prototype.addAtHead = function (val) {
-  let newNode = new Node(5);
+  let newNode = new Node(val);
   newNode.next = this.head;
   this.head = newNode;
   this.size++;
@@ -41,7 +41,7 @@ MyLinkedList.prototype.addAtTail = function (val) {
   let newNode = new Node(val);
   // if LL is empty
   if (this.head == null) {
-    this.head == newNode;
+    this.head = newNode;
   } else {
     let curr = this.head;
     while (curr.next !== null) {
@@ -64,6 +64,9 @@ MyLinkedList.prototype.addAtTail = function (val) {
 
 MyLinkedList.prototype.addAtIndex = function (index, val) {
   let newNode = new Node(val);
+
+  // if index is out of bound
+  if (index < 0 || index > this.size) return;
 
   // if LL is empty
   if (index == 0) {
@@ -91,3 +94,44 @@ MyLinkedList.prototype.addAtIndex = function (index, val) {
 // 4. curr.next = newNode
 // 5. handle corner case
 // 6. increase the size
+
+// _____________________________________
+
+// 6. Get the value at index
+
+MyLinkedList.prototype.get = function (index) {
+  if (index < 0 || index >= this.size) return -1;
+
+  let curr = this.head;
+  for (let i = 0; i < index; i++) {
+    curr = curr.next;
+  }
+  return curr.val;
+};
+
+// _____________________________________
+
+// 7. Delete the value at index
+
+MyLinkedList.prototype.deleteAtIndex = function (index) {
+  if (index < 0 || index >= this.size) return;
+
+  if (index === 0) {
+    this.head = this.head.next;
+  } else {
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+    curr.next = curr.next.next;
+  }
+
+  this.size--;
+};
+
+// 1. reach the (index-1) for deletion
+// 2. link it to the new next node (2 step ahead)
+// 3. handle corner cases
+// 4. reduce the size
+
+// _____________________________________
