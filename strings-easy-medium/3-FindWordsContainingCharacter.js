bd40625bed108b92052c82033120d13f92bfb8e9
simran61 <simrangangwani3107@gmail.com>
2026-09-07 16:10:26 +0530

    find words containing character

---
+++ strings-easy-medium/3-FindWordsContainingCharacter.js
@@ -0,0 +1,50 @@
+// QUES: Find Words Containing Character
+
+// You are given a 0-indexed array of strings words and a character x.
+
+// Return an array of indices representing the words that contain the character x.
+
+// Note that the returned array may be in any order.
+
+// Example 1:
+// Input: words = ["leet","code"], x = "e"
+// Output: [0,1]
+// Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+
+// Example 2:
+// Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+// Output: [0,2]
+// Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+
+// Example 3:
+// Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+// Output: []
+// Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+
+// APPROACH 1
+var findWordsContaining = function (words, x) {
+  let result = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].includes(x)) {
+      result.push(i);
+    }
+  }
+  return result;
+};
+
+// APPROACH 2: without using in-built JS functions
+var findWordsContaining = function (words, x) {
+  let result = [];
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      if (words[i][j] === x) {
+        result.push(i);
+        break;
+      }
+    }
+  }
+  return result;
+};
+
+// TIME COMPLEXITY: m= no of words, n= max length of each word => O(n*m)
+// SPACE COMPLEXITY: O(1)
